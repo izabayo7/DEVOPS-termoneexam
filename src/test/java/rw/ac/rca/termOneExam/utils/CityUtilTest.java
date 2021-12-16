419cfd04e5eecddb4ec0093409796855927470e9
izabayo7 <cedricizabayo7@gmail.com>
2021-12-16 16:12:05 +0200

    finished question 7

--- src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
+++ src/test/java/rw/ac/rca/termOneExam/utils/CityUtilTest.java
@@ -3,6 +3,7 @@ package rw.ac.rca.termOneExam.utils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import rw.ac.rca.termOneExam.domain.City;
 import rw.ac.rca.termOneExam.repository.ICityRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,8 +28,8 @@ public class CityUtilTest {
     public void noCityHasWeatherMoreThan40() {
         boolean result = false;
         List<City> cities = cityRepository.findAll();
-        for (City city: cities)
-            if(city.getWeather() > 40)
+        for (City city : cities)
+            if (city.getWeather() > 40)
                 result = true;
 
 
@@ -38,8 +40,8 @@ public class CityUtilTest {
     public void noCityHasWeatherLessThan10() {
         boolean result = false;
         List<City> cities = cityRepository.findAll();
-        for (City city: cities)
-            if(city.getWeather() < 10)
+        for (City city : cities)
+            if (city.getWeather() < 10)
                 result = true;
 
 
@@ -53,4 +55,23 @@ public class CityUtilTest {
         assertEquals(true, cityRepository.existsByName("Musanze") && cityRepository.existsByName("Kigali"));
     }
 
+    @Test
+    public void testMocking() {
+        List<City> mockedList = Mockito.mock(ArrayList.class);
+        City city = new City("Kigali", 24);
+        mockedList.add(city);
+        Mockito.verify(mockedList).add(city);
+
+        assertEquals(0, mockedList.size());
+    }
+
+    @Test
+    public void testSpying() {
+        List<City> spyList = Mockito.spy(ArrayList.class);
+        City city = new City("Kigali", 24);
+        spyList.add(city);
+        Mockito.verify(spyList).add(city);
+
+        assertEquals(1, spyList.size());
+    }
 }
